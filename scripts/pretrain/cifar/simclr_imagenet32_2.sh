4e68b1d6620d2f26eb42cf362dd42106e2bd48ce
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-04 17:21:14 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_2.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_2.sh
@@ -6,6 +6,7 @@ python3 main_pretrain.py \
     --dataset ${DATASET} \
     --backbone resnet50 \
     --train_data_path /share/wenzhuoliu/torch_ds/imagenet/train  \
+    --val_data_path /share/wenzhuoliu/torch_ds/imagenet/val  \
     --max_epochs 1000 \
     --devices 4,5,6,7 \
     --data_format dali \
