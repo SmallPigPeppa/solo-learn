e94951969d385ce9ed0d3f29e98e03654d2af533
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 21:52:04 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_cluster.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_cluster.sh
@@ -17,6 +17,7 @@ DATASET=imagenet32
     --cifar_path ${CIFAR_PATH} \
     --max_epochs 1000 \
     --devices 0,1,2,3,4,5,6,7 \
+    --accelerator gpu \
     --data_format dali \
     --accelerator gpu \
     --strategy ddp \
@@ -27,11 +28,11 @@ DATASET=imagenet32
     --eta_lars 0.02 \
     --exclude_bias_n_norm \
     --scheduler warmup_cosine \
-    --lr 2.4 \
+    --lr 3.4 \
     --classifier_lr 0.1 \
     --weight_decay 1e-6 \
-    --batch_size 1024 \
-    --num_workers 4 \
+    --batch_size 2048 \
+    --num_workers 16 \
     --crop_size 32 \
     --brightness 0.8 \
     --contrast 0.8 \
