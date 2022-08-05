4f354856ca23f0d72beba90578d2328379d51d8e
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 16:33:01 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_3.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_3.sh
@@ -15,7 +15,7 @@ python3 main_pretrain.py \
     --eval_on_cifar \
     --cifar_path ${CIFAR_PATH} \
     --max_epochs 1000 \
-    --devices 0,1,2,3 \
+    --devices 0,1,2,3,4,5,6,7 \
     --data_format dali \
     --accelerator gpu \
     --strategy ddp \
