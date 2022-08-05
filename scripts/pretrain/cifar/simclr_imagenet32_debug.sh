f62ee0bbdcba17157335ab98917018acf7b95cf8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 22:31:52 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_debug.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_debug.sh
@@ -10,10 +10,9 @@ python3 main_pretrain.py \
     --train_data_path /share/wenzhuoliu/torch_ds/imagenet/train  \
     --val_data_path /share/wenzhuoliu/torch_ds/imagenet/val  \
     --max_epochs 1000 \
-    --devices 0,1 \
+    --devices 0,1,2,3,4,5,6,7 \
     --data_format dali \
     --accelerator gpu \
-    --dali_device cpu\
     --strategy ddp \
     --sync_batchnorm \
     --precision 16 \
