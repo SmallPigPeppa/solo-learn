751c2dd053815fba8610f8f61e9e9d226f9658f8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 17:26:19 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_debug.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_debug.sh
@@ -25,7 +25,7 @@ python3 main_pretrain.py \
     --classifier_lr 0.1 \
     --weight_decay 1e-5 \
     --batch_size 512 \
-    --num_workers 16 \
+    --num_workers 4 \
     --crop_size 32 \
     --brightness 0.8 \
     --contrast 0.8 \
