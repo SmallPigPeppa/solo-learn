bc6128d653f11776b9aad6bcb991684e23a640ad
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 16:18:17 +0800

    initial

--- scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_cluster.sh
+++ scripts/pretrain/cifar/simclr_imagenet32_eval_on_cifar_cluster.sh
@@ -30,7 +30,7 @@ DATASET=imagenet32
     --lr 2.4 \
     --classifier_lr 0.1 \
     --weight_decay 1e-6 \
-    --batch_size 256 \
+    --batch_size 1024 \
     --num_workers 4 \
     --crop_size 32 \
     --brightness 0.8 \
