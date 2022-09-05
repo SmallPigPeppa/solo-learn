5b9fd6e156cbc4629f72718a05c3ecef077b8134
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-05 22:01:56 +0800

    initial

---
+++ scripts/pretrain/imagenet/byol_cluster.sh
@@ -0,0 +1,44 @@
+cd /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn
+DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+/root/miniconda3/envs/solo-learn/bin/python main_pretrain.py \
+    --dataset imagenet \
+    --backbone resnet50 \
+    --train_data_path ${DATA_PATH}/train  \
+    --val_data_path ${DATA_PATH}/val  \
+    --max_epochs 1000 \
+    --devices 0,1,2,3,4,5,6,7 \
+    --accelerator gpu \
+    --strategy ddp \
+    --sync_batchnorm \
+    --precision 16 \
+    --optimizer lars \
+    --eta_lars 0.001 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.45 \
+    --accumulate_grad_batches 16 \
+    --classifier_lr 0.2 \
+    --weight_decay 1e-6 \
+    --batch_size 128 \
+    --num_workers 4 \
+    --data_format dali \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --gaussian_prob 1.0 0.1 \
+    --solarization_prob 0.0 0.2 \
+    --num_crops_per_aug 1 1 \
+    --name byol-imagenet \
+    --project solo-learn \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint \
+    --auto_resume \
+    --method byol \
+    --proj_output_dim 256 \
+    --proj_hidden_dim 4096 \
+    --pred_hidden_dim 4096 \
+    --base_tau_momentum 0.99 \
+    --final_tau_momentum 1.0 \
+    --momentum_classifier
