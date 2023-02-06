552dac10e6994cc2a21a2194f7f6fc68fa86523d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-06 17:12:37 +0800

    initial

--- scripts/pretrain/cifar100/simclr_cifar100_eval_on_cifar10.sh
+++ scripts/pretrain/cifar100/simclr_cifar100_eval_on_cifar10.sh
@@ -1,7 +1,9 @@
 cd /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn
+#git pull origin
 DATA_PATH=/mnt/mmtech01/dataset/lzy/ILSVRC2012
+#CIFAR_PATH=/mnt/mmtech01/dataset/wenzhuoliu/torch_ds
 CIFAR_PATH=/mnt/mmtech01/usr/liuwenzhuo/torch_ds
-DATASET=imagenet32
+DATASET=cifar100
 #    --val_data_path /share/wenzhuoliu/torch_ds/imagenet/val  \
 # 0.075* sqrt(batch_size)
 #    --weight_decay 1e-5 \ resnet18
@@ -9,44 +11,44 @@ DATASET=imagenet32
 #    --eval_on_cifar \
 /root/miniconda3/envs/solo-learn/bin/python main_pretrain.py \
     --dataset ${DATASET} \
-    --backbone resnet50 \
-    --train_data_path ${DATA_PATH}/train  \
-    --val_data_path ${DATA_PATH}/val  \
+    --backbone resnet18 \
+    --train_data_path ${CIFAR_PATH}  \
+    --val_data_path ${CIFAR_PATH}  \
     --eval_on_cifar \
     --cifar_path ${CIFAR_PATH} \
     --max_epochs 1000 \
-    --devices 0,1,2,3,4,5,6,7 \
+    --devices 0,1 \
     --accelerator gpu \
     --data_format dali \
     --strategy ddp \
     --sync_batchnorm \
     --precision 16 \
     --optimizer lars \
+    --grad_clip_lars \
     --eta_lars 0.02 \
     --exclude_bias_n_norm \
     --scheduler warmup_cosine \
-    --lr 0.3 \
-    --classifier_lr 0.3 \
-    --weight_decay 1e-6 \
-    --batch_size 512 \
+    --lr 0.4 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-5 \
+    --batch_size 256 \
     --num_workers 4 \
-    --brightness 0.4 \
-    --contrast 0.4 \
-    --saturation 0.4 \
-    --hue 0.1 \
-    --gaussian_prob 1.0 0.1 \
-    --solarization_prob 0.0 0.2 \
-    --min_scale 0.2 \
+    --crop_size 32 \
+    --brightness 0.8 \
+    --contrast 0.8 \
+    --saturation 0.8 \
+    --hue 0.2 \
+    --gaussian_prob 0.0 0.0 \
     --crop_size 32 \
     --num_crops_per_aug 1 1 \
-    --name mocov3-${DATASET} \
+    --name simclr-${DATASET} \
     --project solo-learn \
     --entity pigpeppa \
+    --wandb \
     --save_checkpoint \
     --auto_resume \
-    --method mocov3 \
-    --proj_hidden_dim 4096 \
-    --pred_hidden_dim 4096 \
+    --method simclr \
     --temperature 0.2 \
-    --base_tau_momentum 0.99 \
-    --final_tau_momentum 1.0
+    --proj_hidden_dim 2048 \
+    --proj_output_dim 256
+
