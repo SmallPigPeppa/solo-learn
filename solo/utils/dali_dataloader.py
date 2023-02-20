4e986652ba32859810de6be341c872a67fa6467d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-20 20:53:24 +0800

    initial

--- solo/utils/dali_dataloader.py
+++ solo/utils/dali_dataloader.py
@@ -534,6 +534,108 @@ class CustomTransform:
     def __repr__(self):
         return self.str
 
+class CustomMultiTransform:
+    def __init__(
+        self,
+        device: str,
+        brightness: float,
+        contrast: float,
+        saturation: float,
+        hue: float,
+        color_jitter_prob: float = 0.8,
+        gray_scale_prob: float = 0.2,
+        horizontal_flip_prob: float = 0.5,
+        gaussian_prob: float = 0.5,
+        solarization_prob: float = 0.0,
+        min_scale: float = 0.08,
+        max_scale: float = 1.0,
+        crop_size: int = 224,
+        mean: Sequence[float] = (0.485, 0.456, 0.406),
+        std: Sequence[float] = (0.228, 0.224, 0.225),
+    ):
+        # cifar 100
+        # mean = (0.5071, 0.4865, 0.4409)
+        # std = (0.2673, 0.2564, 0.2762)
+        """Applies Custom transformations.
+        If you want to do exoteric augmentations, you can just re-write this class.
+
+        Args:
+            device (str): device on which the operations will be performed.
+            brightness (float): sampled uniformly in [max(0, 1 - brightness), 1 + brightness].
+            contrast (float): sampled uniformly in [max(0, 1 - contrast), 1 + contrast].
+            saturation (float): sampled uniformly in [max(0, 1 - saturation), 1 + saturation].
+            hue (float): sampled uniformly in [-hue, hue].
+            gaussian_prob (float, optional): probability of applying gaussian blur. Defaults to 0.5.
+            solarization_prob (float, optional): probability of applying solarization. Defaults
+                to 0.0.
+            min_scale (float, optional): minimum scale of the crops. Defaults to 0.08.
+            max_scale (float, optional): maximum scale of the crops. Defaults to 1.0.
+            crop_size (int, optional): size of the side of the image after transformation. Defaults
+                to 224.
+            mean (Sequence[float], optional): mean values for normalization.
+                Defaults to (0.485, 0.456, 0.406).
+            std (Sequence[float], optional): std values for normalization.
+                Defaults to (0.228, 0.224, 0.225).
+        """
+
+        # random crop
+        self.random_crop = ops.RandomResizedCrop(
+            device=device,
+            size=crop_size,
+            random_area=(min_scale, max_scale),
+            interp_type=types.INTERP_CUBIC,
+        )
+
+        # color jitter
+        self.random_color_jitter = RandomColorJitter(
+            brightness=brightness,
+            contrast=contrast,
+            saturation=saturation,
+            hue=hue,
+            prob=color_jitter_prob,
+            device=device,
+        )
+
+        # grayscale conversion
+        self.random_grayscale = RandomGrayScaleConversion(prob=gray_scale_prob, device=device)
+
+        # gaussian blur
+        self.random_gaussian_blur = RandomGaussianBlur(prob=gaussian_prob, device=device)
+
+        # solarization
+        self.random_solarization = RandomSolarize(prob=solarization_prob)
+
+        # normalize and horizontal flip
+        self.cmn = ops.CropMirrorNormalize(
+            device=device,
+            dtype=types.FLOAT,
+            output_layout=types.NCHW,
+            mean=[v * 255 for v in mean],
+            std=[v * 255 for v in std],
+        )
+        self.coin05 = ops.random.CoinFlip(probability=horizontal_flip_prob)
+
+        self.str = (
+            "CustomTransform("
+            f"random_crop({min_scale}, {max_scale}), "
+            f"random_color_jitter(brightness={brightness}, "
+            f"contrast={contrast}, saturation={saturation}, hue={hue}), "
+            f"random_gray_scale, random_gaussian_blur({gaussian_prob}), "
+            f"random_solarization({solarization_prob}), "
+            "crop_mirror_resize())"
+        )
+
+    def __call__(self, images):
+        out = self.random_crop(images)
+        out = self.random_color_jitter(out)
+        out = self.random_grayscale(out)
+        out = self.random_gaussian_blur(out)
+        out = self.random_solarization(out)
+        out = self.cmn(out, mirror=self.coin05())
+        return out
+
+    def __repr__(self):
+        return self.str
 
 class PretrainPipelineBuilder:
     def __init__(
@@ -850,7 +952,7 @@ class PretrainDALIDataModule(pl.LightningDataModule):
         elif dataset in ['imagenet32']:
             transform_pipeline = CustomTransform
         elif dataset in ['multi-scale-imagenet']:
-            transform_pipeline = MultiCustomTransform
+            transform_pipeline = CustomMultiTransform
         elif dataset == "custom":
             transform_pipeline = CustomTransform
         else:
