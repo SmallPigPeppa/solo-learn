d8fc3535eb0fb05f386dbb3b8d5279e1d67eb9ea
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-07 00:20:34 +0800

    initial

--- scripts/pretrain/cifar10/barlow.sh
+++ scripts/pretrain/cifar10/barlow.sh
@@ -28,7 +28,6 @@ python3 main_pretrain.py \
     --project solo-learn-cifar10 \
     --entity pigpeppa \
     --wandb \
-    --offline \
     --save_checkpoint \
     --auto_resume \
     --method barlow_twins \
