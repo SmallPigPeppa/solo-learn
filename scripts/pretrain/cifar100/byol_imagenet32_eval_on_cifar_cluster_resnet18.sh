a2075a68f29980da354cc3f335294bfb5cf94208
lwz <905328134@qq.com>
2023-12-07 16:17:04 +0800

    initial

--- scripts/pretrain/cifar100/byol_imagenet32_eval_on_cifar_cluster_resnet18.sh
+++ scripts/pretrain/cifar100/byol_imagenet32_eval_on_cifar_cluster_resnet18.sh
@@ -25,7 +25,7 @@ DATASET=imagenet32
     --accumulate_grad_batches 16 \
     --classifier_lr 0.1 \
     --weight_decay 1e-5 \
-    --batch_size 256 \
+    --batch_size 1024 \
     --num_workers 4 \
     --brightness 0.4 \
     --contrast 0.4 \
